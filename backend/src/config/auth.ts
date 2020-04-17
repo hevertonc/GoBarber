9f702eabb27f632ebeb26065127e55af9cb57b32
Heverton Carneiro <hevertoncarneiro@MacBook-Pro-de-Heverton-3.local>
2020-04-16 22:23:56 -0300

    CHORE: Upload n Error Manager

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  jwt: {
+    secret:
+      'e62eb0e0166de3a7c98a918db89ca5a5d9bd5ba5ae3360a14d72ee558e6b3fb0f9d41ec31cc38401e444b2896087d3e187d0f6ea50327869887ff533bcb60f00',
+    expiresIn: '1d',
+  },
+};
