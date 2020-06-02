f08d1302c8ef5bc5bff5fb65c4d8e018b5ee42d7
Heverton Carneiro <hevertoncarneiro@MacBook-Pro-de-Heverton-3.local>
2020-06-02 02:42:42 -0300

    CHORE: Invalidando Cache

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
@@ -20,6 +21,8 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -36,6 +39,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
